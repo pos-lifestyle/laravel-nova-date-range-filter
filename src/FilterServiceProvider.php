7e206f4c63855d8325f4306f6f9a9ed9942f7667
Jan Hähne <github@jhae.de>
2019-11-03 13:55:24 +0100

    Remove placeholder translation
    Doesn't work as expected. Translation is still possible via config or
    the Laravel Nova translation files.

--- src/FilterServiceProvider.php
+++ src/FilterServiceProvider.php
@@ -10,19 +10,12 @@ class FilterServiceProvider extends ServiceProvider
 {
     protected const JS_FILE = __DIR__ . '/../dist/js/filter.js';
     protected const CSS_FILE = __DIR__ . '/../dist/css/filter.css';
-    protected const LANGUAGE_FILES = [
-        'de' => __DIR__ . '/../resources/lang/de/filter.json',
-    ];
 
     public function boot(): void
     {
         Nova::serving(static function () {
             Nova::script('date-range-filter', self::JS_FILE);
             Nova::style('date-range-filter', self::CSS_FILE);
-
-            foreach (self::LANGUAGE_FILES as $languageFile) {
-                Nova::translations($languageFile);
-            }
         });
     }
 }
