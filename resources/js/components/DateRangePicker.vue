b21bffa64afd19efc326ced6e6d702b264da54cd
Jonathan Smithies <jonathan@elev8stud.io>
2021-04-22 17:40:07 +0100

    Reverted updated indentation

--- resources/js/components/DateRangePicker.vue
+++ resources/js/components/DateRangePicker.vue
@@ -1,142 +1,142 @@
 <template>
-  <input
-      type="text"
-      ref="dateRangePicker"
-      :disabled="disabled"
-      :placeholder="placeholder"
-      :value="value"
-  />
+    <input
+        type="text"
+        ref="dateRangePicker"
+        :disabled="disabled"
+        :placeholder="placeholder"
+        :value="value"
+    />
 </template>
 
 <script>
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/themes/airbnb.css';
+    import flatpickr from 'flatpickr'
+    import 'flatpickr/dist/themes/airbnb.css'
 
-export default {
-  props: {
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-    placeholder: {
-      type: String,
-      default: () => {
-        return moment().format('YYYY-MM-DD');
-      },
-    },
-    value: {
-      required: false,
-    },
-    allowInput: {
-      type: Boolean,
-      default: false,
-    },
-    dateFormat: {
-      type: String,
-      default: 'Y-m-d',
-    },
-    enableTime: {
-      type: Boolean,
-      default: false,
-    },
-    enableSeconds: {
-      type: Boolean,
-      default: false,
-    },
-    locale: {
-      type: Object | String,
-      default: 'default',
-    },
-    maxDate: {
-      type: String,
-      default: null,
-    },
-    minDate: {
-      type: String,
-      default: null,
-    },
-    shorthandCurrentMonth: {
-      type: Boolean,
-      default: false,
-    },
-    showMonths: {
-      type: Number,
-      default: 1,
-    },
-    time24hr: {
-      type: Boolean,
-      default: true,
-    },
-    weekNumbers: {
-      type: Boolean,
-      default: false,
-    },
-    firstDayOfWeek: {
-      type: Number,
-      default: 0,
-    },
-  },
+    export default {
+        props: {
+            disabled: {
+                type: Boolean,
+                default: false,
+            },
+            placeholder: {
+                type: String,
+                default: () => {
+                    return moment().format('YYYY-MM-DD')
+                },
+            },
+            value: {
+                required: false,
+            },
+            allowInput: {
+                type: Boolean,
+                default: false,
+            },
+            dateFormat: {
+                type: String,
+                default: 'Y-m-d',
+            },
+            enableTime: {
+                type: Boolean,
+                default: false,
+            },
+            enableSeconds: {
+                type: Boolean,
+                default: false,
+            },
+            locale: {
+                type: Object | String,
+                default: 'default',
+            },
+            maxDate: {
+                type: String,
+                default: null,
+            },
+            minDate: {
+                type: String,
+                default: null,
+            },
+            shorthandCurrentMonth: {
+                type: Boolean,
+                default: false,
+            },
+            showMonths: {
+                type: Number,
+                default: 1,
+            },
+            time24hr: {
+                type: Boolean,
+                default: true,
+            },
+            weekNumbers: {
+                type: Boolean,
+                default: false,
+            },
+            firstDayOfWeek: {
+                type: Number,
+                default: 0,
+            },
+        },
 
-  data: function () {
-    return {
-      flatpickr: null,
-    };
-  },
-
-  mounted: function () {
-    this.$nextTick(() => {
-      this.flatpickr = flatpickr(this.$refs.dateRangePicker, {
-        allowInput: this.allowInput,
-        dateFormat: this.dateFormat,
-        enableTime: this.enableTime,
-        enableSeconds: this.enableSeconds,
-        locale: this.locale === 'default'
-            ? {
-              firstDayOfWeek: this.firstDayOfWeek,
-              time_24hr: this.time24hr,
+        data: function () {
+            return {
+                flatpickr: null,
             }
-            : this.locale,
-        maxDate: this.maxDate,
-        minDate: this.minDate,
-        mode: 'range',
-        shorthandCurrentMonth: this.shorthandCurrentMonth,
-        showMonths: this.showMonths,
-        time_24hr: this.time24hr,
-        weekNumbers: this.weekNumbers,
-        onChange: this.onChange,
-      });
+        },
 
-      this.$emit('ready', true);
-    });
-  },
+        mounted: function () {
+            this.$nextTick(() => {
+                this.flatpickr = flatpickr(this.$refs.dateRangePicker, {
+                    allowInput: this.allowInput,
+                    dateFormat: this.dateFormat,
+                    enableTime: this.enableTime,
+                    enableSeconds: this.enableSeconds,
+                    locale: this.locale === 'default'
+                        ? {
+                            firstDayOfWeek: this.firstDayOfWeek,
+                            time_24hr: this.time24hr,
+                        }
+                        : this.locale,
+                    maxDate: this.maxDate,
+                    minDate: this.minDate,
+                    mode: 'range',
+                    shorthandCurrentMonth: this.shorthandCurrentMonth,
+                    showMonths: this.showMonths,
+                    time_24hr: this.time24hr,
+                    weekNumbers: this.weekNumbers,
+                    onChange: this.onChange,
+                })
 
-  beforeDestroy: function () {
-    this.flatpickr.destroy();
-  },
+                this.$emit('ready', true)
+            })
+        },
 
-  methods: {
-    onChange: function (selectedDates) {
-      if (selectedDates.length === 1) {
-        return;
-      }
+        beforeDestroy: function () {
+            this.flatpickr.destroy()
+        },
 
-      this.$emit('change', selectedDates);
-    },
+        methods: {
+            onChange: function (selectedDates) {
+                if (selectedDates.length === 1) {
+                    return
+                }
 
-    getFlatpickrConfig: function () {
-      return this.flatpickr.config;
-    },
+                this.$emit('change', selectedDates)
+            },
 
-    clear: function () {
-      this.flatpickr.clear();
-    },
-  },
-};
+            getFlatpickrConfig: function () {
+                return this.flatpickr.config
+            },
+
+            clear: function () {
+                this.flatpickr.clear()
+            },
+        },
+    }
 </script>
 
 <style scoped>
-/* 'cursor: not-allowed' removed because the result is misleading -
- * when hovering the input field, the not-allowed cursor suggests
- * the field is not clickable, but you can still click it.
- */
+    /* 'cursor: not-allowed' removed because the result is misleading -
+     * when hovering the input field, the not-allowed cursor suggests
+     * the field is not clickable, but you can still click it.
+     */
 </style>
