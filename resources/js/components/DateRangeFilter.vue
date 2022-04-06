02b9da1663e92532ea96e3a5a7be40bfa8290771
Yaniv Harush <harushy@gmail.com>
2022-04-06 18:34:36 +0300

    wip

--- resources/js/components/DateRangeFilter.vue
+++ resources/js/components/DateRangeFilter.vue
@@ -1,153 +1,204 @@
 <template>
-	<Input
-		type="text"
-		ref="dateRangePicker"
-		:disabled="disabled"
-		:placeholder="placeholder"
-		:value="value"
-	/>
+	<FilterContainer>
+		<span>{{ filter.name }}</span>
+
+		<template #filter>
+			<div class="relative">
+				<input type="text" autofocus="autofocus" style="display:none" />
+				<date-range-picker
+					:dusk="`${filter.name}`"
+					name="date-range-filter"
+					ref="dateRangePickerComponent"
+					class="w-full form-control form-input form-input-bordered"
+					:class="{'pr-10' : isValidCurrentValue}"
+					style="cursor: pointer !important;"
+					autocomplete="off"
+					:disabled="disabled"
+					:placeholder="placeholder"
+					:allow-input="allowInput"
+					:date-format="dateFormat"
+					:enable-time="enableTime"
+					:enable-seconds="enableSeconds"
+					:locale="locale"
+					:value="value"
+					:max-date="maxDate"
+					:min-date="minDate"
+					:shorthand-current-month="shorthandCurrentMonth"
+					:show-months="showMonths"
+					:time24hr="time24hr"
+					:week-numbers="weekNumbers"
+					:first-day-of-week="firstDayOfWeek"
+					@input.prevent=""
+					@change="debouncedHandleChange"
+					@ready="setDisplayValue"
+				/>
+
+				<div v-show="isValidCurrentValue"
+					 class="absolute inset-y-0 right-0 top-0 p-2 z-20 flex items-center">
+					<button @click="resetFilter" class="text-gray-400">
+						<icon type="x-circle" view-box="0 0 22 22" width="22" height="22" />
+					</button>
+				</div>
+			</div>
+		</template>
+	</FilterContainer>
 </template>
 
 <script>
-import flatpickr from 'flatpickr'
-import 'flatpickr/dist/themes/airbnb.css'
-import moment    from 'moment/moment'
+import debounce        from 'lodash/debounce'
+import DateRangePicker from './DateRangePicker'
+import moment          from 'moment/moment'
+import flatpickr       from 'flatpickr'
 
 export default {
+	components: { DateRangePicker },
 
-	emits: ['ready', 'change'],
+	emits: ['change'],
 
 	props: {
-		disabled             : {
-			type   : Boolean,
-			default: false,
+		resourceName: {
+			type    : String,
+			required: true,
 		},
-		placeholder          : {
-			type   : String,
-			default: () => {
-				return moment().format('YYYY-MM-DD')
-			},
+		filterKey   : {
+			type    : String,
+			required: true,
 		},
-		value                : {
-			required: false,
-		},
-		allowInput           : {
-			type   : Boolean,
-			default: false,
-		},
-		dateFormat           : {
-			type   : String,
-			default: 'Y-m-d',
-		},
-		enableTime           : {
-			type   : Boolean,
-			default: false,
-		},
-		enableSeconds        : {
-			type   : Boolean,
-			default: false,
-		},
-		locale               : {
-			type   : Object | String,
-			default: 'default',
-		},
-		maxDate              : {
-			type   : String,
-			default: null,
-		},
-		minDate              : {
-			type   : String,
-			default: null,
-		},
-		shorthandCurrentMonth: {
-			type   : Boolean,
-			default: false,
-		},
-		showMonths           : {
-			type   : Number,
-			default: 1,
-		},
-		time24hr             : {
-			type   : Boolean,
-			default: true,
-		},
-		weekNumbers          : {
-			type   : Boolean,
-			default: false,
-		},
-		firstDayOfWeek       : {
-			type   : Number,
-			default: 0,
-		},
+		lens        : String,
 	},
 
-	data: function () {
-		return {
-			flatpickr: null,
-		}
+	data: () => ( {
+		displayValue         : '',
+		debouncedHandleChange: null,
+	} ),
+
+	created () {
+		this.debouncedHandleChange = debounce((value) => this.handleChange(value), 100)
 	},
 
-	mounted: function () {
-		this.$nextTick(() => {
-			this.flatpickr = flatpickr(this.$refs.dateRangePicker, {
-				position: 'bottom left',
-				positionElement: this.$refs.dateRangePicker,
-				allowInput           : this.allowInput,
-				dateFormat           : this.dateFormat,
-				enableTime           : this.enableTime,
-				enableSeconds        : this.enableSeconds,
-				locale               : this.locale === 'default'
-					? {
-						firstDayOfWeek: this.firstDayOfWeek,
-						time_24hr     : this.time24hr,
-					}
-					: this.locale,
-				maxDate              : this.maxDate,
-				minDate              : this.minDate,
-				mode                 : 'range',
-				shorthandCurrentMonth: this.shorthandCurrentMonth,
-				showMonths           : this.showMonths,
-				time_24hr            : this.time24hr,
-				weekNumbers          : this.weekNumbers,
-				onChange             : this.onChange,
-			})
-
-			this.$emit('ready', this.flatpickr)
-		})
+	mounted () {
+		Nova.$on('filter-reset', this.resetFilter)
 	},
 
-	beforeDestroy: function () {
-		this.flatpickr.destroy()
+	beforeUnmount () {
+		Nova.$off('filter-reset', this.resetFilter)
 	},
 
 	methods: {
+		handleChange (value) {
 
-		moment () {
-			return moment()
+			/*
+			 * Remove repetition and provide empty value when clear button is pressed.
+			 * This tells Nova that the filter is no longer enabled, changing the filter
+			 * icon colour from blue to regular colour. Without this fix, you can press
+			 * the clear button, but the filter icon still looks as if a filter is applied.
+			 */
+			value = Array.isArray(value) && value.length === 2
+				? value.map((value) => moment(value).format('YYYY-MM-DD'))
+				: []
+
+			this.$store.commit(`${ this.resourceName }/updateFilterState`, {
+				filterClass: this.filterKey,
+				value      : value,
+			})
+
+			this.setDisplayValue()
+
+			this.$emit('change')
 		},
 
-		onChange(selectedDates) {
-			if (selectedDates.length === 1) {
+		setDisplayValue () {
+			const flatpickrConfig = this.$refs.dateRangePickerComponent.getFlatpickrConfig()
+			const rangeSeparator = flatpickrConfig.locale.rangeSeparator || 'to'
+
+			if (!this.isValidCurrentValue) {
 				return
 			}
 
-			this.$emit('change', selectedDates)
+			this.displayValue = this.filter.currentValue.map((value) => flatpickr.formatDate(flatpickr.parseDate(value), this.dateFormat)).join(` ${ rangeSeparator } `)
 		},
 
-		getFlatpickrConfig () {
-			return this.flatpickr.config
+		resetFilter () {
+			this.$refs.dateRangePickerComponent.clear()
+			this.displayValue = ''
+		},
+	},
+
+	computed: {
+		filter () {
+			return this.$store.getters[ `${ this.resourceName }/getFilter` ](
+				this.filterKey,
+			)
 		},
 
-		clear () {
-			this.flatpickr.clear()
+		isValidCurrentValue () {
+			return Array.isArray(this.filter.currentValue) && this.filter.currentValue.length === 2
+		},
+
+		disabled: function () {
+			return this.filter.disabled !== undefined ? this.filter.disabled : false
+		},
+
+		placeholder: function () {
+			return this.filter.placeholder || this.__('Choose date range')
+		},
+
+		value: function () {
+			return this.displayValue
+		},
+
+		allowInput: function () {
+			return this.filter.allowInput !== undefined ? this.filter.allowInput : false
+		},
+
+		dateFormat: function () {
+			return this.filter.dateFormat || 'Y-m-d'
+		},
+
+		enableTime: function () {
+			return this.filter.enableTime !== undefined ? this.filter.enableTime : false
+		},
+
+		enableSeconds: function () {
+			return this.filter.enableSeconds !== undefined ? this.filter.enableSeconds : false
+		},
+
+		locale: function () {
+			// There is no english language file.
+			if (!this.filter.locale || this.filter.locale === 'en') {
+				return 'default'
+			}
+
+			return require(`flatpickr/dist/l10n/${ this.filter.locale }.js`).default[ this.filter.locale ]
+		},
+
+		maxDate: function () {
+			return this.filter.maxDate || null
+		},
+
+		minDate: function () {
+			return this.filter.minDate || null
+		},
+
+		shorthandCurrentMonth: function () {
+			return this.filter.shorthandCurrentMonth !== undefined ? this.filter.shorthandCurrentMonth : false
+		},
+
+		showMonths: function () {
+			return this.filter.showMonths || 1
+		},
+
+		time24hr: function () {
+			return this.filter.time24hr !== undefined ? this.filter.time24hr : false
+		},
+
+		weekNumbers: function () {
+			return this.filter.weekNumbers !== undefined ? this.filter.weekNumbers : false
+		},
+
+		firstDayOfWeek: function () {
+			return this.filter.firstDayOfWeek || 0
 		},
 	},
 }
 </script>
-
-<style scoped>
-/* 'cursor: not-allowed' removed because the result is misleading -
- * when hovering the input field, the not-allowed cursor suggests
- * the field is not clickable, but you can still click it.
- */
-</style>
